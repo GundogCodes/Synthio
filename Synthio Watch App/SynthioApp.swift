8563f885598eae825328207f1f599c2978eaa30c
Gunish Sharma <gunishsharma20@gmail.com>
2025-08-16 21:24:27 -0600

    Initial Commit

---
+++ Synthio Watch App/SynthioApp.swift
@@ -0,0 +1,17 @@
+//
+//  SynthioApp.swift
+//  Synthio Watch App
+//
+//  Created by Gunish Sharma on 2025-08-16.
+//
+
+import SwiftUI
+
+@main
+struct Synthio_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
